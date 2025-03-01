9aa92df56b993aea413c31d1ea8f22d7bdfca25a
Shadid123 <shadidmazumder123@gmail.com>
2025-03-01 22:10:56 +0600

    How  to take user input

---
+++ User_input.py
@@ -0,0 +1,7 @@
+name=str (input("Enter your name:"))
+size=len(name)
+print("The size of your name is:",len(name))
+""" id=int (input("Enter your id:"))
+CGPA=float(input("Enter your CGPA:"))
+
+print("Name:"+name+ "\nID:",id,"\nCGPA:",CGPA) """
