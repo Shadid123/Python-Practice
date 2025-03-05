1707a51d69c81b3b3ba8f2bf601209d95ff18822
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-06 00:17:25 +0600

    Class & Obejcts in Python

---
+++ Class_and_Object.py
@@ -0,0 +1,14 @@
+class Student:
+    roll=''
+    gpa=''
+Rahim=Student()
+Rahim.roll=25
+Rahim.gpa=3.50
+print(f"Roll:{Rahim.roll}\nGpa:{Rahim.gpa}")
+
+Karim=Student()
+Karim.roll=26
+Karim.gpa=3.56
+print(f"Roll:{Karim.roll}\nGpa:{Karim.gpa}")
+
+
