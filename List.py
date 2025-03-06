3cab5ba056678e622e8d54c62b58ff9d845c3781
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-07 00:05:23 +0600

    List in Python

---
+++ List.py
@@ -0,0 +1,17 @@
+subjects=["English","Urdu","Math","Science","Science"]
+subjects2=subjects.copy()
+print(subjects)
+
+
+
+""" Output:['English', 'Urdu', 'Math', 'Science']"""
+""" numbers=[25,45,87,95,35,68]
+numbers.sort()
+print(numbers) """
+""" subjects.remove("Urdu")
+print(subjects)
+ """
+""" subjects.insert(2,"Hindi")
+print(subjects) """
+subjects.append ("Urdu")
+print(subjects)
