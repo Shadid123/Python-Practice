f597c4bdffd11749f761d427215f0957eade2167
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-18 00:28:14 +0600

    Added a Python script demonstrating the use of map() with a function and lambda for squaring a list of numbers.

---
+++ Map-Function.py
@@ -0,0 +1,13 @@
+def  square(x):
+   return x*x
+
+Num=[1,2,3,4,5]
+
+result=list(map(square,Num))
+print(result)
+
+
+""" Using thge  lambda  funtion
+result=list(map(lambda x:x*x,Num))
+print(result) """
+
