2486a0c63a0c6f4dde615efdac5d750b2e68d2f8
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-20 19:47:00 +0600

    Added arithmetic operations using functions in Python

---
+++ Function.py
@@ -0,0 +1,23 @@
+def add(x,y):
+    sum=x+y
+    print(f"Sum of {x} and {y} is:{sum}")
+def sub(x,y):
+    sub=x-y
+    print(f"Substraction of {x} and {y} is:{sub}")
+def mul(x,y):
+    mul=x*y
+    print(f"Multiplication of {x} and {y} is:{mul}")
+def div(x,y):
+    div=x//y
+    print(f"Division of {x} and {y} is:{div}")
+
+
+a=int(input("Enter  the first number:"))
+b=int(input("Enter the second number:"))
+
+
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
+
