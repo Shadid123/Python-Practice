d2bc3263df8c52d17cb6723cbf43e21c39ac757c
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-22 16:08:17 +0600

    Add Own_module.py with area calculation functions and Accessing_Module.py for execution

---
+++ Accessing_Module.py
@@ -0,0 +1,3 @@
+from Own_module import *
+rectangle(20,30)
+triangle(20,30)
---
+++ Own_module.py
@@ -0,0 +1,7 @@
+def triangle(b,h):
+    print(f"Area  of triangle is:{0.5*b*h}")
+
+def rectangle(b,h):
+    print(f"Area of  rectangle is:{b*h}")
+    
+    
