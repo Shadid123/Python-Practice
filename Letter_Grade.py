391e16f010f8a45731c50b96e477efcd368a16f3
Shadid Mazumder <shadidmazumder123@gmail.com>
2025-03-23 14:48:34 +0600

    Added a Python program to determine grades based on marks

---
+++ Letter_Grade.py
@@ -0,0 +1,13 @@
+mark =int(87)
+if 80<=mark<=100:
+    print("A+")
+elif 70<=mark<=79:
+    print("A")
+elif 60<=mark<=69:
+    print("B")
+elif 50<=mark<=59:
+    print("C")
+elif 40<=mark<=49:
+    print("D")
+else:
+    print("F")
